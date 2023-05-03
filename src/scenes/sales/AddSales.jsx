c43e81330e9adceaf887e610663b23fb15b84cc1
Monisha A <monishaa7000@gmail.com>
2023-05-03 11:00:40 +0530

    scenes-Sales

--- src/scenes/sales/AddSales.jsx
+++ src/scenes/sales/AddSales.jsx
@@ -4,6 +4,7 @@ import Footer from "../../layouts/Footer";
 import { useState } from 'react';
 import { Button, Card, Col, Nav, ProgressBar, Row, Form } from "react-bootstrap";
 import { Link, useNavigate } from 'react-router-dom'
+import Sales from './Sales';
 
 function AddSales() {
   // to maintain dark and light mode
@@ -11,62 +12,103 @@ function AddSales() {
   const [skin, setSkin] = useState(currentSkin);
   const navigate = useNavigate()
 
+  const [form, setform] = useState("")
+  const onChangeHandler = (event) => {
+    setform({
+      ...form,
+      [event.target.name]: event.target.value
+    })
+  }
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(form);
+  }
 
   return (
     <>
-      <Header onSkin={setSkin} />
-      <div className="main main-app p-3 p-lg-4">
-        <div className="d-md-flex align-items-center justify-content-between mb-4">
-          <div>
-            <ol className="breadcrumb fs-sm mb-1">
-              <li className="breadcrumb-item"><Link to="/dashboard/home">Dashboard</Link></li>
-              <li className="breadcrumb-item"><Link to="/dashboard/sales">Sales</Link></li>
-              <li className="breadcrumb-item active" aria-current="page">Add Sales</li>
-            </ol>
-            <h4 className="main-title mb-0">Create New HR</h4>
+      <form onSubmit={onSubmitHandler}>
+        <Header onSkin={setSkin} />
+        <div className="main main-app p-3 p-lg-4">
+          <div className="d-md-flex align-items-center justify-content-between mb-4">
+            <div>
+              <ol className="breadcrumb fs-sm mb-1">
+                <li className="breadcrumb-item"><Link to="/dashboard/home">Dashboard</Link></li>
+                <li className="breadcrumb-item"><Link to="/dashboard/crm">Invoice</Link></li>
+                <li className="breadcrumb-item active" aria-current="page">Add Invoice</li>
+              </ol>
+              <h4 className="main-title mb-0">Create New Sales</h4>
+            </div>
           </div>
-        </div>
 
-        {/* responsive form in a card */}
-        <Card>
-          <Card.Body>
-            <Row className="g-4">
-              <Col xl="4" md="6" xs="12">
-                <div className="mt-3">
-                  <Form.Label htmlFor="exampleFormControlInput1">Email address</Form.Label>
-                  <Form.Control type="email" id="exampleFormControlInput1" placeholder="name@example.com" />
-                </div>
-              </Col>
+          {/* responsive form in a card */}
+          <Card>
+            <Card.Body>
+              <Row className="g-4">
+                <Col lg="4" md="6" xs="12">
+                  <div className="mt-3">
+                    <Form.Label htmlFor="OrderNumber">Order Number</Form.Label>
+                    <Form.Control type="Number" id="OrderNumber" name="OrderNumber" placeholder="Order Number" onChange={onChangeHandler} />
+                  </div>
+                </Col>
 
-              <Col lg="4" md="6" xs="12">
-                <div className="mt-3">
-                  <Form.Label htmlFor="exampleFormControlInput1">Email address</Form.Label>
-                  <Form.Control type="email" id="exampleFormControlInput1" placeholder="name@example.com" />
-                </div>
-              </Col>
+                <Col lg="4" md="6" xs="12">
+                  <div className="mt-3">
+                    <Form.Label htmlFor="Product">Product</Form.Label>
+                    <Form.Control type="text" id="Product" name='Product' placeholder="Product" onChange={onChangeHandler} />
+                  </div>
+                </Col>
 
-              <Col lg="4" md="6" xs="12">
-                <div className="mt-3">
-                  <Form.Label htmlFor="exampleFormControlInput1">Email address</Form.Label>
-                  <Form.Control type="email" id="exampleFormControlInput1" placeholder="name@example.com" />
-                </div>
-              </Col>
+                <Col lg="4" md="6" xs="12">
+                  <div className="mt-3">
+                    <Form.Label htmlFor="Day">Day</Form.Label>
+                    <Form.Control type="Day" id="Day" name='Day' placeholder="Day" onChange={onChangeHandler} />
+                  </div>
+                </Col>
 
-              <Col lg="3" md="6" xs="12">
-                <div className="mt-3">
-                  <Form.Label htmlFor="exampleFormControlInput1">Email address</Form.Label>
-                  <Form.Control type="email" id="exampleFormControlInput1" placeholder="name@example.com" />
-                </div>
-              </Col>
+                <Col lg="4" md="6" xs="12">
+                  <div className="mt-3">
+                    <Form.Label htmlFor="Month">Month</Form.Label>
+                    <Form.Control type="Month" id="Month" name='Month' placeholder="Month" onChange={onChangeHandler} />
+                  </div>
+                </Col>
 
-              <Col sx="12">
-                <Button>Submit</Button>
-              </Col>
-            </Row>
-          </Card.Body>
-        </Card>
-        <Footer />
-      </div>
+                <Col lg="4" md="6" xs="12">
+                  <div className="mt-3">
+                    <Form.Label htmlFor="Year">Year</Form.Label>
+                    <Form.Control type="Year" id="Year" name='Year' placeholder="Year" onChange={onChangeHandler} />
+                  </div>
+                </Col>
+
+                <Col lg="4" md="6" xs="12">
+                  <div className="mt-3">
+                    <Form.Label htmlFor="DueDate">Status</Form.Label>
+                    <Form.Control type="text" id="Status" name='Status' placeholder="Status" onChange={onChangeHandler} />
+                  </div>
+                </Col>
+
+                <Col lg="4" md="6" xs="12">
+                  <div className="mt-3">
+                    <Form.Label htmlFor="TotalAmount">Total Amount</Form.Label>
+                    <Form.Control type="Number" id="TotalAmount" name='TotalAmount' placeholder="Total Amount" onChange={onChangeHandler} />
+                  </div>
+                </Col>
+
+                <Col xs="12">
+                  <Button type='submit'>Submit</Button>
+                </Col>
+              </Row>
+
+
+
+            </Card.Body>
+          </Card>
+
+          <Footer />
+
+        </div >
+      </form>
+
     </>
   )
 }
