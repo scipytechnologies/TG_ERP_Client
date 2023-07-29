d26b920e293791338ffaba90113d71afb6709188
Adharsh Maptell <adharsh@maptell.com>
2023-07-29 12:29:02 +0530

    test commit

---
+++ src/scenes/employee/EmployeeProfile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function EmployeeProfile() {
+
+    const currentSkin = (localStorage.getItem('skin-mode')) ? 'dark' : '';
+  return (
+    <div>EmployeeProfile</div>
+  )
+}
+
+export default EmployeeProfile
